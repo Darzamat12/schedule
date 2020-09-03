fe7efc525f6c2c3ad9c4c59e5287d10273b81ba5
Pavel <alexgromik@gmail.com>
2020-09-03 16:51:01 +0300

    feat: add basic filters

---
+++ src/utils/filters.js
@@ -0,0 +1,53 @@
+export const filters = {
+    author: [
+        {
+            text: "admin",
+            value: "admin",
+        },
+        {
+            text: "mentor",
+            value: "mentor",
+        },
+        {
+            text: "another_mentor",
+            value: "another_mentor",
+        },{
+            text: "article",
+            value: "article",
+        },
+    ],
+    tag: [
+        {
+            text: "interactive",
+            value: "interactive",
+        },
+        {
+            text: "article",
+            value: "article",
+        },
+        {
+            text: "video",
+            value: "video",
+        },
+        {
+            text: "cv task",
+            value: "cv task",
+        },
+        {
+            text: "html/css task",
+            value: "html/css task",
+        },
+        {
+            text: "codewars",
+            value: "codewars",
+        },
+        {
+            text: "js task",
+            value: "js task",
+        },
+        {
+            text: "interview",
+            value: "interview",
+        },
+    ],
+}
