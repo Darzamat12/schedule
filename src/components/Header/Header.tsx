8bde3ffeaabbe5bc4042fdb9c6cbba67f442e575
Demchuk Tatyana <69cobak@mail.ru>
2020-09-06 18:51:19 +0300

    feat: add icons for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,31 @@
 import React from 'react';
+import './Header.less'
+import Icon from '@ant-design/icons';
+
+
 import {Layout, Button, Avatar} from 'antd';
 import {LeftOutlined,  SettingFilled} from '@ant-design/icons'
+import {LeftArrow, Setting} from '../Icons/Icons'
 
-const {Header} = Layout;
-import './Header.less'
-function List() {
+function Header() {
   return (
-    <Header className="header">
+    <header className="header">
       <a href="#">
         <img className="logo" src="https://app.rs.school/static/images/logo-rsschool3.png"/>
       </a>
       <h1 className="title">Schedule</h1>
-      <div>
+      <div className = "header__buttons">
           <Button className="header__setting" size="large">
-            <LeftOutlined />
-            <SettingFilled/>
+            <LeftArrow />
+            <Setting/>
           </Button>
         <Button size="large" type="dashed">
         <Avatar className="header__avatar" size="small"  src="https://app.rs.school/static/images/logo-rsschool3.png" />
           My profile
         </Button>
       </div>
-    </Header>
+    </header>
   );
 }
 
-export default List; 
+export default Header; 
---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function LeftArrow(){
+    return(
+        <object type="image/svg+xml" data="./src/icons/arrow.svg"/>
+    )
+}
+
+export function Setting(){
+    return(
+        <object type="image/svg+xml" data="./src/icons/Vector.svg"/>
+    )
+}
