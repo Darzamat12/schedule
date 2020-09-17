bcc67ad28aa434cf745f5946295e5b0fc2893a35
Darzamat12 <heavylesha@gmail.com>
2020-09-17 23:37:42 +0300

    fix variable types

--- src/components/Calendar/DataFuncs.ts
+++ src/components/Calendar/DataFuncs.ts
@@ -1,17 +1,14 @@
 import { filterByDate } from './DateFuncs';
 
 export function getListData(value, props) {
-  let listData = [];
-  filterByDate(props, value).forEach((el) => {
+  const listData: { type: string; content: any; key: any; }[] = [];
+  filterByDate(props, value).forEach((el: { name: any; id: any; }) => {
     listData.push({ type: 'success', content: el.name, key: el.id });
   });
 
   return listData || [];
 }
 
-export function isEmpty(obj) {
-  for (let key in obj) {
-    return false;
-  }
-  return true;
+export function isEmpty(obj: {}) {
+  return Object.keys(obj).length === 0;
 }
