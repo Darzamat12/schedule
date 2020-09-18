db464a9e197696317432f3845279e715f77b3587
keksik77 <lukashevich7tim@gmail.com>
2020-09-18 04:34:06 +0300

    feat: add improved list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,32 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import {fetchScheduleData} from '../../redux/actions';
+import React, { useEffect, useMemo, useState } from 'react';
+import { fetchScheduleData } from '../../redux/actions';
 import { connect } from 'react-redux';
-import { List, message, Avatar, Spin, Checkbox, Tag} from 'antd';
+import { List, Avatar, Spin, Checkbox, Tag, Alert, Pagination } from 'antd';
 import moment from 'moment';
 import './List.less';
 import { Scrollbars } from 'react-custom-scrollbars';
 import { LinkOutlined } from '@ant-design/icons';
-
-
-interface Event {
-  id: number;
-  name: string;
-  author: string;
-  tag: string;
-  date: string;
-  deadline: string;
-  duration: number;
-  description: string;
-  result: string;
-  remark: string;
-  links: Array<string>;
-  photo: null;
-  video: string;
-  map: string;
-  rating: number;
-  feedback: string;
-}
-
+import useWindowDimensions from '../../utils/useWindowDimensions';
+import TaskPageDrawer from '../TaskPageDrawer';
+import { Event } from './types';
 
 const ListView = (props: any /*plug*/) => {
   useEffect(() => {
@@ -35,71 +17,133 @@ const ListView = (props: any /*plug*/) => {
     }
   }, []);
 
+  const [renderIndex, setRenderIndex] = useState(0);
+  const { width } = useWindowDimensions();
 
-  let DateArray = [];
-  if (props.data !== null) {
-    DateArray = props.data.filter((item: Event, index: number, arr: Event[]) => {
-      const thisDate = moment(item.date).format('YYYY-MM-DD');
-      const prevDate = ((index - 1) >= 0) ? moment(arr[index - 1].date).format('YYYY-MM-DD') : null;
-      return prevDate !== thisDate;
-  });}
+  const [drawerCurrentItem, setDrawerCurrentItem] = useState(Object);
+  const [drawerIsShow, setDrawerIsShow] = useState(false);
+  const handleOnCloseDrawer = () => {
+    setDrawerIsShow(false);
+  };
 
+  const shortData = useMemo(() => {
+    if (props.data !== null) {
+      return props.data.slice(renderIndex * 11, renderIndex * 11 + 11).map((elem: Event) => {
+        const date = new Date(elem.date);
+        date.setHours(date.getHours() - (3 /*Moscow time offset*/ - props.timeZone));
+        return { ...elem, date: date };
+      });
+    } else {
+      return null;
+    }
+  }, [renderIndex, props.data, props.timeZone]);
 
+  const DateArray = useMemo(() => {
+    if (shortData != null) {
+      return shortData.filter((item: Event, index: number, arr: Event[]) => {
+        const thisDate = moment(item.date).format('YYYY-MM-DD');
+        const prevDate = index - 1 >= 0 ? moment(arr[index - 1].date).format('YYYY-MM-DD') : null;
+        return prevDate !== thisDate;
+      });
+    } else {
+      return [];
+    }
+  }, [shortData]);
+
+  const handlerPaginationChange = (current: number) => {
+    setRenderIndex(current - 1);
+  };
+
+  const renderEventItem = (eventItem: Event) => {
+    return (
+      <List.Item key={eventItem.id} className={'list-item'}>
+        <div className={'left-list-item-info'}>
+          <Checkbox />
+          <div className="main-list-item-info">
+            <h3>{eventItem.name}</h3>
+            <div className="description-of-list-item">
+              <Tag key={eventItem.tag}>{eventItem.tag}</Tag>
+              <p>{'Time: ' + moment(eventItem.date).format('HH:mm')}</p>
+              <p>
+                {eventItem.links.map((link) => {
+                  return (
+                    <a key={link} href={link} title={link}>
+                      <LinkOutlined />
+                    </a>
+                  );
+                })}
+              </p>
+              <div className="organizer-container">
+                <p>Organizer: {eventItem.author}</p>
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="more-info-container">
+          <a
+            onClick={() => {
+              setDrawerCurrentItem(eventItem);
+              setDrawerIsShow(true);
+            }}
+          >
+            more
+          </a>
+          {props.isAdmin && <a>edit</a>}
+        </div>
+      </List.Item>
+    );
+  };
 
   return (
-    <>
-      {props.loading && <p>Loading...</p>}
-      {props.error && <p>Error, try again</p>}
-      {props.data !== null && DateArray !== [] &&
+    <div className="list-wrapper">
+      {props.loading && (
+        <div className={'list-spin'}>
+          <Spin size={'large'} />
+        </div>
+      )}
+
+      {props.error && <Alert message="Error" description="Error while loading data." type="error" showIcon />}
+
+      {props.data !== null && DateArray !== [] && (
         <>
-          <Scrollbars className={'scrollbar-list-container'} style={{height: 700 }}>
-          <div style={{overflowX: 'hidden'}}>
-          {
-            DateArray.map((item: Event, index: number) => {
-              const thisDate = moment(item.date).format('YYYY-MM-DD');
-              return <div key={index} className={'list-events-container'}>
-                <p className={'list-date-wrapper'}>{moment(item.date).format('MMMM Do YYYY')}</p>
-                <List
-                  dataSource={props.data}
-                  renderItem={(eventItem: Event, index) => {
-                    if (thisDate === moment(eventItem.date).format('YYYY-MM-DD')) {
-                      return <List.Item key={eventItem.name + index.toString()} className={'list-item'}>
-                          <div className={'left-list-item-info'}>
-                            <Checkbox/>
-                            <div className='main-list-item-info'>
-                              <h3>{eventItem.name}</h3>
-                              <p style={{marginBottom: 0,}}>{eventItem.result}</p>
-                              <div className='description-of-list-item'>
-                                <Tag key={eventItem.tag}>{eventItem.tag}</Tag>
-                                <p>{'Time: ' + moment(eventItem.date).format('HH:mm')}</p>
-                                <p>{eventItem.links.map((link) => {
-                                  return (
-                                  <a key={link} href={link} title={link}>
-                                    <LinkOutlined />
-                                  </a>
-                                )})}</p>
-                                <div className='organizer-container'>
-                                  <p>Organizer: {eventItem.author}</p>
-                                  <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div className='more-info-container'>
-                            <a>more</a>
-                          </div>
-                      </List.Item>
-                    }
-                  }}
-                >
-                </List>
-              </div>
-            })
-          }
-          </div></Scrollbars>
+          <Scrollbars className={'scrollbar-list-container'} style={{ height: 650 }}>
+            <div style={{ overflowX: 'hidden' }}>
+              {DateArray.map((item: Event, index: number) => {
+                const thisDate = moment(item.date).format('YYYY-MM-DD');
+                return (
+                  <div key={index} className={'list-events-container'}>
+                    <p className={'list-date-wrapper'}>{moment(item.date).format('MMMM Do YYYY')}</p>
+                    <List
+                      dataSource={shortData}
+                      renderItem={(eventItem: Event) => {
+                        if (thisDate === moment(eventItem.date).format('YYYY-MM-DD')) {
+                          return renderEventItem(eventItem);
+                        }
+                      }}
+                    ></List>
+                  </div>
+                );
+              })}
+              <TaskPageDrawer
+                isShown={drawerIsShow}
+                handleOnClose={handleOnCloseDrawer}
+                currentItem={drawerCurrentItem}
+              ></TaskPageDrawer>
+            </div>
+          </Scrollbars>
+
+          <Pagination
+            showSizeChanger={false}
+            defaultPageSize={11}
+            total={props.data.length}
+            onChange={handlerPaginationChange}
+            className={'list-pagination'}
+            size={width <= 500 ? 'small' : 'default'}
+          />
         </>
-      }
-  </>
+      )}
+    </div>
   );
 };
 
@@ -108,7 +152,9 @@ const mapStateToProps = (state: any) => {
     loading: state.scheduleData.loading,
     error: state.scheduleData.error,
     data: state.scheduleData.data,
-    //isAdmin: state.userModeData.userMode,
+    isAdmin: state.userMode.isAdmin,
+    timeZone: state.timeZoneData.timeOffset,
+    week: state.weekPickerData.week,
   };
 };
 
