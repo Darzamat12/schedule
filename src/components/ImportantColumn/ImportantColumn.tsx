4c92618c67edd7e39f203b3abca3c4a980e905e4
Demchuk Tatyana <69cobak@mail.ru>
2020-09-22 19:43:40 +0300

    feat: make changes

--- src/components/ImportantColumn/ImportantColumn.tsx
+++ src/components/ImportantColumn/ImportantColumn.tsx
@@ -2,48 +2,29 @@ import { Table, Button, Space } from 'antd';
 import {useState} from 'react';
 import React from 'react'
 import ScheduleData from '../../data/scheduleData.json'
-
-const importantWrapper = {
-  height: "100%",
-  width: "100%",
-  cursor: "pointer",
-}
-
-const importantIco = {
-  height: "20px",
-  width: "20px",
-  backgroundColor: "#888",
-  borderRadius: "100%",
-}
-
-const importantIcoActive = {
-  height: "20px",
-  width: "20px",
-  backgroundColor: "red",
-  borderRadius: "100%",
-}
-
-
+import './ImportantColumn.less'
 
 export const Check = ({value, id}) =>{
-  const [ state, setState] = useState({
-    checked: value,
-    activeClass: value ? importantIcoActive : importantIco,
-    key: id,
+  const [ state, setState] = useState(()=>{
+    return {
+      checked: value ? true : false,
+      activeClass: value ? "active" : "",
+    }
   })
   const onClick = ()=> {
     let index = ScheduleData.findIndex(el => el.id == id )
-    setState(({checked, key})=>({
+    setState(({checked})=>{
+      return{
       ...state,
       checked: !checked,
-      activeClass: !checked ? importantIcoActive : importantIco,
-    }))
-    ScheduleData[index].important = state.checked
-    console.log(ScheduleData[index], state.checked)
+      activeClass: !checked ? "active" : "",
+    }})
+    ScheduleData[index].important = !state.checked
+    console.log(ScheduleData[index].important, !state.checked)
   }
   return (
-    <div style= {importantWrapper} onClick={()=>onClick()}>
-      <div style={state.activeClass}/>
+    <div className={"importantWrapper"} onClick={()=>onClick()}>
+      <div className={`importantIco ${state.activeClass}`} />
     </div>
   )
 }
@@ -73,34 +54,17 @@ export class Important extends React.Component {
     });
   };
 
-  setAgeSort = () => {
-    this.setState({
-      sortedInfo: {
-        order: 'descend',
-        columnKey: 'age',
-      },
-    });
-  };
-
   render() {
     let { sortedInfo, filteredInfo } = this.state;
     sortedInfo = sortedInfo || {};
     filteredInfo = filteredInfo || {};
     const columns = [
-
       {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'address' && sortedInfo.order,
-        ellipsis: true,
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => {
+          return <a>{text}</a>},
       },
       {
         title: '!',
@@ -108,10 +72,8 @@ export class Important extends React.Component {
         key: 'important',
         filters: [
           { text: 'ok', value: true },
-          { text: 'no', value: false },
         ],
         render: (value, record)=> {
-          console.log(value)
           return <Check value={value} id={record.id}/>
         },
         filteredValue: filteredInfo.important || null,
