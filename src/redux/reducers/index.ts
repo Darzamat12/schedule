93253d250e65115c7719b8905d16584e2c7946c8
Pavel <alexgromik@gmail.com>
2020-09-26 20:37:54 +0300

    fix: typo fix

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -24,7 +24,7 @@ export default combineReducers({
     taskPageReducer1: TaskPageReducer,
     timeZoneData: timeZoneReducer,
     weekPickerData: weekPickerReducer,
-    hideColumnData: hideColumsReducer,
+    hideColumnData: hideColumnsReducer,
     importantColData: importantReducer,
     initColumnsData: initColumnsReducer,
     postEvent: postEventReducer,
